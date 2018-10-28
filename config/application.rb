632ccbc9d15f7c5fca4a36b2b1691662abfe6899
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2018-10-28 02:29:54 -0300

    chore(generators): Avoid unnecessary generators

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,17 @@ module Factual
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Do not generate specs for views and requests. Also, do not generate assets
     config.generators do |g|
       g.template_engine = :slim
+      g.helper false
+      g.view_specs false
+      g.assets false
+      g.integration_tool false
+    end
+
+    config.app_generators do |g|
+      g.test_framework :rspec
     end
 
     # Settings in config/environments/* take precedence over those
@@ -22,5 +31,9 @@ module Factual
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Prevent initializing your application and connect to the database
+    # on assets precompile.
+    config.assets.initialize_on_precompile = false
   end
 end
