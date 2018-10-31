31ab0e95ca065e7f92da64397727e5a62cb36ce5
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2018-10-31 07:43:44 -0300

    chore(search): Add ransack gem
    An Object-based searching.
    
    More: https://github.com/activerecord-hackery/ransack

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@
 
 require_relative 'boot'
 
+require File.expand_path('../boot', __FILE__)
+ENV['RANSACK_FORM_BUILDER'] = '::SimpleForm::FormBuilder'
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
