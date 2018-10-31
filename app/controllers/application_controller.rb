93823482a016106fca8686953ce37f07542b353b
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2018-10-31 05:18:12 -0300

    chore(controllers): Add responders
    A set of Rails responders to dry up the application.
    
    More: https://github.com/plataformatec/responders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
+require 'application_responder'
+
 class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :html
 end
