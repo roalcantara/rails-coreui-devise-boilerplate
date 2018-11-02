a8df1844c481b19624e8af5a4aeb6921258301b4
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2018-11-02 19:04:14 -0300

    chore(ui): Add kaminari gem
    A Scope & Engine based, clean, powerful, customizable and sophisticated
    paginator for Ruby webapps.
    
    Also:
    - Add kaminari-i18n gem;
    
    More: https://github.com/kaminari/kaminari

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,17 @@ require 'application_responder'
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
+
+  before_action :set_page, only: %i[index]
+  before_action :set_per_page, only: %i[index]
+
+  private
+
+  def set_page
+    @page = params[:page] || 1
+  end
+
+  def set_per_page
+    @per_page = params[:per_page] || 10
+  end
 end
