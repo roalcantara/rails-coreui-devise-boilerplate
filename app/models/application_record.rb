a15024210985d21684214910cd345a97cb978596
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2018-11-04 02:59:47 -0200

    feat(models): Add enum methods
    Which returns:
    
    - All humanized enum values;
    - The current humanized enum value from instance;

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -14,4 +14,20 @@ class ApplicationRecord < ActiveRecord::Base
       end
     end
   end
+
+  def self.human_enum_values_to_h(enum)
+    Hash[send(enum.to_s.pluralize).map { |key, _| [key, human_enum_value(enum, key)] }]
+  end
+
+  def self.human_enum_values_to_a(enum)
+    send(enum.to_s.pluralize).to_a.map { |w| [human_enum_value(enum, w[0]), w[1]] }
+  end
+
+  def self.human_enum_value(enum, value)
+    human_attribute_name("#{enum}.#{value}")
+  end
+
+  def human_enum_value(enum, value = send(enum))
+    self.class.human_enum_value(enum, value) unless value.blank?
+  end
 end
