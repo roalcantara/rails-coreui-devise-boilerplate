9aac2cd5a63ff93409ff426bfa2d930fb8686ab9
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2018-11-04 02:58:48 -0200

    feat(models): Add to_csv class method
    Which receives records and column names and generates and returns a CSV from them.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+require 'csv'
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.to_csv(records = [], attributes = column_names)
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      records.each do |record|
+        csv << attributes.map { |attr| record.send(attr) }
+      end
+    end
+  end
 end
